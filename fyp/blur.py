ac27b85a7d8a5ec14e7a048d43a3870298f0a4a4
Zhixiang Zhang <894695171@qq.com>
2018-11-20 14:46:50 +0800

    Add files via upload

--- fyp/blur.py
+++ fyp/blur.py
@@ -1,4 +1,5 @@
 # pink - [192,128,128,255]
+import os
 import math
 import numpy as np
 from PIL import Image
@@ -12,11 +13,14 @@ class Blur():
         self.sigema=sigema
         self.sideLength=radius*2+1
     #Gaussian Function
+    
+    
     def calc(self,x,y):
         #权重的计算公式，xy为高斯模板中的点的坐标
         res1=1/(2*math.pi*self.sigema*self.sigema)
         res2=math.exp(-(x*x+y*y)/(2*self.sigema*self.sigema))
         return res1*res2
+    
     
     #get Gassian template
     def template(self):
@@ -37,8 +41,8 @@ class Blur():
         # template is the Gassian template which will be used to blur the image
         new = np.array(image) #这里用两个nparray去存原图片的像素rgb信息
         raw = np.array(image) #是为了让先进行颜色替换的像素不会影响到其他像素求替换后的值
-        maxRow = image.shape[0]-self.radius-1 #filter全在image内的px的最大行引索值
-        maxColumn = image.shape[1]-self.radius-1 #filter全在image内的px的最大列引索值
+        maxRow = raw.shape[0]-self.radius-1 #filter全在image内的px的最大行引索值
+        maxColumn = raw.shape[1]-self.radius-1 #filter全在image内的px的最大列引索值
         
         for position in mask:
             row = position[0]
@@ -48,29 +52,29 @@ class Blur():
                 if row < self.radius:#filter上方有几行px会定位在图片之外
                     if column < self.radius:#左 上
                         for i in range(self.radius - column):#filter前r-column列的px，列数为[0,r-column-1]
-                            for j in range (sideLength):
+                            for j in range (self.sideLength):
                                 if j < (self.radius - row):#行数为[0,r-row-1]的px
                                     #镜像
                                     t[j][i]=raw[row+self.radius-j][column+self.radius-i][k]
                                 else: #垂直轴对称 行数为[r-row,sideLength-1]的px
                                     t[j][i]=raw[row-self.radius+j][column+self.radius-i][k]
-                        for i in range(self.radius - column,sideLength):#filter第r-column列之后的px,列数为[r-column,sideLength-1]
-                            for j in range (sideLength):
+                        for i in range(self.radius - column,self.sideLength):#filter第r-column列之后的px,列数为[r-column,sideLength-1]
+                            for j in range (self.sideLength):
                                 if j <(self.radius - row):#行数为[0,r-row-1]的px
                                     #水平轴对称
                                     t[j][i]=raw[row+self.radius-j][column-self.radius+i][k]
                                 else:#直接取  行数为[r-row,sideLength-1]的px
                                     t[j][i]=raw[row-self.radius+j][column-self.radius+i][k]
                     elif column > maxColumn:#右 上
-                        for i in range(sideLength - (column - maxColumn)):#px的列数为[0,sideLength-(column-maxColumn)-1]
-                            for j in range (sideLength):
+                        for i in range(self.sideLength - (column - maxColumn)):#px的列数为[0,sideLength-(column-maxColumn)-1]
+                            for j in range (self.sideLength):
                                 if j < (self.radius - row):#行数为[0,r-row-1]的px
                                     #水平轴对称
                                     t[j][i]=raw[row+self.radius-j][column-self.radius+i][k]
                                 else: #直接取
                                     t[j][i]=raw[row-self.radius+j][column-self.radius+i][k]
-                        for i in range(sideLength - (column - maxColumn),sideLength):#px的列数为[sideLength-(column-maxColumn),sideLength-1]
-                            for j in range (sideLength):
+                        for i in range(self.sideLength - (column - maxColumn),self.sideLength):#px的列数为[sideLength-(column-maxColumn),sideLength-1]
+                            for j in range (self.sideLength):
                                 if j <(self.radius - row):#行数为[0,r-row-1]的px
                                     #镜像
                                     t[j][i]=raw[row+self.radius-j][column+self.radius-i][k]
@@ -87,38 +91,38 @@ class Blur():
                 elif row > maxRow:#filter下方有几行px会定位在图片之外
                     if column < self.radius:#左 下
                         for i in range(self.radius - column):#filter前r-column列的px，列数为[0,r-column-1]
-                            for j in range (sideLength):
-                                if j < (sideLength - (row - maxRow)-1):#px的行数为[0,sideLength-(row-maxRow)-1]
+                            for j in range (self.sideLength):
+                                if j < (self.sideLength - (row - maxRow)-1):#px的行数为[0,sideLength-(row-maxRow)-1]
                                     #垂直轴对称
                                     t[j][i]=raw[row-self.radius+j][column+self.radius-i][k]
                                 else: #镜像 #px的行数为[sideLength-(row-maxRow),sideLength-1]
                                     t[j][i]=raw[row+self.radius-j][column+self.radius-i][k]
-                        for i in range(self.radius - column,sideLength):#filter第r-column之后列的px,列数为[r-column,sideLength-1]
-                            for j in range (sideLength):
-                                if j <(sideLength - (row - maxRow)-1):#px的行数为[0,sideLength-(row-maxRow)-1]
+                        for i in range(self.radius - column,self.sideLength):#filter第r-column之后列的px,列数为[r-column,sideLength-1]
+                            for j in range (self.sideLength):
+                                if j <(self.sideLength - (row - maxRow)-1):#px的行数为[0,sideLength-(row-maxRow)-1]
                                     #直接取
                                     t[j][i]=raw[row-self.radius+j][column-self.radius+i][k]
                                 else:#水平轴对称 #px的行数为[sideLength-(row-maxRow),sideLength-1]
                                     t[j][i]=raw[row+self.radius-j][column-self.radius+i][k]
                     elif column > maxColumn:#右 下
-                        for i in range(sideLength - (column - maxColumn)):#px的列数为[0,sideLength-(column-maxColumn)-1]
-                            for j in range (sideLength):
-                                if j <(sideLength - (row - maxRow)-1):#px的行数为[0,sideLength-(row-maxRow)-1]
+                        for i in range(self.sideLength - (column - maxColumn)):#px的列数为[0,sideLength-(column-maxColumn)-1]
+                            for j in range (self.sideLength):
+                                if j <(self.sideLength - (row - maxRow)-1):#px的行数为[0,sideLength-(row-maxRow)-1]
                                     #直接取
                                     t[j][i]=raw[row-self.radius+j][column-self.radius+i][k]
                                 else: #水平轴对称 #px的行数为[sideLength-(row-maxRow),sideLength-1]
                                     t[j][i]=raw[row+self.radius-j][column-self.radius+i][k]
-                        for i in range(sideLength - (column - maxColumn),sideLength):#px的列数为[sideLength-(column-maxColumn),sideLength-1]
-                            for j in range (sideLength):
-                                if j <(sideLength - (row - maxRow)-1):#px的行数为[0,sideLength-(row-maxRow)-1]
+                        for i in range(self.sideLength - (column - maxColumn),self.sideLength):#px的列数为[sideLength-(column-maxColumn),sideLength-1]
+                            for j in range (self.sideLength):
+                                if j <(self.sideLength - (row - maxRow)-1):#px的行数为[0,sideLength-(row-maxRow)-1]
                                     #垂直轴对称
                                     t[j][i]=raw[row-self.radius+j][column+self.radius-i][k]
                                 else:#镜像 #px的行数为[sideLength-(row-maxRow),sideLength-1]
-                                t[j][i]=raw[row+self.radius-j][column+self.radius-i][k]
+                                    t[j][i]=raw[row+self.radius-j][column+self.radius-i][k]
                     else:#仅下
                         for i in range(self.sideLength):
                             for j in range(self.sideLength):
-                                if j < (sideLength - (row - maxRow)-1):
+                                if j < (self.sideLength - (row - maxRow)-1):
                                     t[j][i]=raw[row-self.radius+j][column-self.radius+i][k]
                                 else:
                                     t[j][i]=raw[row+self.radius-j][column-self.radius+i][k]
@@ -131,28 +135,47 @@ class Blur():
         return newImage
 
     def getMask(self,maskPath):
-      img = Image.open(maskPath)
-      mask = np.array(img)
-      #get the position of pixel whose rgb value == xxx
-      height = mask.shape[0]
-      width = mask.shape[1]
-      positions = []
-      for i in range(height):
-        for j in range(width):
-          rgb = mask[i][j]
-          if rgb[0] == 192 and rgb[1] ==128 and rgb[2] == 128 and rgb[3] == 255:
-            positions.append((i,j))
-      return positions
+        f = open(maskPath,'rb')
+        img = Image.open(f)
+        mask = np.array(img)
+        #get the position of pixel whose rgb value == xxx
+        height = mask.shape[0]
+        width = mask.shape[1]
+        positions = []
+        for i in range(height):
+            for j in range(width):
+                rgb = mask[i][j]
+                if rgb[0] == 192 and rgb[1] ==128 and rgb[2] == 128 and rgb[3] == 255:
+                    positions.append((i,j))
+        f.close()
+        return positions
         
-time1 = time.time()
+
 r=7 #模版半径，自己自由调整
 s=50 #sigema数值，自己自由调整
 blur=Blur(radius=r, sigema=s)#声明高斯模糊类
 temp=blur.template()#得到滤波模版
-im=Image.open('v_PlayingTabla_g01_c01.avi(0).bmp')#打开图片
-poss = blur.getMask('v_PlayingTabla_g01_c01.avi(0)_mask.png')
-image=blur.filter(im, poss, temp)#高斯模糊滤波，得到新的图片
-image.show()#图片显示
-time2 = time.time()
-image.save('r=7 s=50.bmp')
-print('二维卷积耗时:' + str(time2-time1))
+segmentation_base = '/media/zzx/DATA/segmentation/'
+frames_base = '/media/zzx/DATA/frames/'
+processed_base = '/media/zzx/DATA/processed/'
+video_types = os.listdir(segmentation_base)
+for video_type in video_types:
+    video_names = os.listdir(segmentation_base + video_type + '/')
+    for video_name in video_names:
+        video_name_f = video_name.split('.txt')[0]
+        video_name_new = video_name.split('..txt')[0]
+        mask_names = os.listdir(segmentation_base + video_type + '/' + video_name + '/')
+            for mask_name in mask_names:
+                frame_name = mask_name.split('_mask')+[0] + '.bmp'
+                f = open(frames_base + '/' + video_type + '/' + video_name_f + '/' + frame_name,'rb')
+                frame = Image.open(f)
+                mask = blur.getMask(segmentation_base + video_type + '/' + video_name + '/' + mask_name)
+                new = processed_base + '/' + video_type + '/' + video_name_new + '/' 
+                image = blur.filter(frame,mask,temp)
+                if not os.path.exists(new):
+                    os.makedirs(new)
+                image.save(new + frame_name)
+                f.close()
+                print('generating---'+ new + frame_name)
+
+                

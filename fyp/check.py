be79f15e4cf21f9872ecb020d34132d89d4c8b69
Zhixiang Zhang <894695171@qq.com>
2019-02-28 14:07:34 +0800

    Update check.py

--- fyp/check.py
+++ fyp/check.py
@@ -1,7 +1,7 @@
 import os
 
-frames_base = '/media/zzx/DATA1/frames/'
-processed_base = '/media/zzx/DATA1/processed/'
+frames_base = '/media/zzx/DATA1/segmentation' #两个需要比较的目录
+processed_base = '/media/zzx/DATA1/processed'
 
 total_types = 0
 total_num = 0
@@ -11,9 +11,9 @@ video_types = os.listdir(frames_base)
 for video_type in video_types:
     sum = 0 #这类视频中错误视频的个数
     list =  [] #错误视频的编号集合
-    video_names = os.listdir(processed_base + '/' +video_type)
+    video_names = os.listdir(frames_base + '/' +video_type)
     for video_name in video_names:
-        ori_len = len(os.listdir(frames_base + '/' +video_type + '/' + video_name + '.'))
+        ori_len = len(os.listdir(frames_base + '/' +video_type + '/' + video_name))
         aft_len = len(os.listdir(processed_base + '/' +video_type + '/' + video_name))
         if (ori_len != aft_len):
             list.append(video_name.split('_')[-2]+video_name.split('_')[-1])
@@ -25,5 +25,5 @@ for video_type in video_types:
         print(list)
         print('\n error number: ' + str(sum) + '\n') #
 
-print('the total number of error video types: ' + total_types)
-print('the total number of error videos:' + total_num)
+print('the total number of error video types: ' + str(total_types))
+print('the total number of error videos:' + str(total_num))

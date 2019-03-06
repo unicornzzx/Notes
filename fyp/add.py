62c50c59068cc121a0cff39f721d52b95eed4431
Zhixiang Zhang <894695171@qq.com>
2019-03-06 14:19:18 +0800

    #生成需要重新语义分割帧的列表

---
+++ fyp/add.py
@@ -0,0 +1,24 @@
+import os
+ 
+frames_base = '/media/zzx/ssd/frames' #两个需要比较的目录
+segmentation_base = '/media/zzx/ssd/segmentation'
+
+f = open('add_list.txt', mode = 'w')
+
+video_types = os.listdir(frames_base)
+
+for video_type in video_types:
+    video_names = os.listdir(os.path.join(frames_base ,video_type))
+    for video_name in video_names:
+        frames = os.listdir(os.path.join(frames_base,video_type,video_name))
+        masks = os.listdir(os.path.join(segmentation_base,video_type,video_name))
+        if (len(frames) != len(masks)):
+            for frame in frames:
+                mask = os.path.join(segmentation_base, video_type, video_name, frame)
+                if !os.path.exist(mask):
+                    f.write(mask)
+                    f.next()
+                    print('--------Add: '+ mask)
+            print('Done: '+ video_name)        
+
+f.close()
